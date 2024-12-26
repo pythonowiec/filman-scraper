c7f34f66e8ebf16de556ad5f9301c4e873a50303
pythonowiec <kuba.dobosz2004@gmail.com>
2024-12-27 00:18:48 +0100

    set up frontend

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', // Allows access from within Sail's container
+        port: 5173, // Ensure this matches your environment
+        hmr: {
+            host: 'localhost', // Or your application's domain
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
